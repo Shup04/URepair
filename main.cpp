1fc630227cf532dc1d0c2587a5cc1a3ea52f5c51
Shup04 <Bradleyschmidt04@gmail.com>
2024-10-30 13:49:59 -0700

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+
+int main() {
+    std::cout << "Hello World." << std::endl;
+    return 0;
+}
